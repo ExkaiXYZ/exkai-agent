898454c82673a8260c04873f59cea44107f2d059
ExkaiXYZ <strco1231@gmail.com>
2026-07-08 11:25:10 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import GlobalNetwork from "@/components/GlobalNetwork";
+import QueryConsole from "@/components/QueryConsole";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <GlobalNetwork />
+      <QueryConsole />
+    </>
+  );
+}
